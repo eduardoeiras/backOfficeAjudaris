04bc2fba7421cfddbf188960aad133fa07c1b5df
eduardoeiras <61152075+eduardoeiras@users.noreply.github.com>
2021-05-31 10:45:31 +0100

    Conclusão da migração e retorno para a página de projetos

--- app/Http/Controllers/MigracaoController.php
+++ app/Http/Controllers/MigracaoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\ExcelImport;
+use App\Models\Projeto;
+use DB;
 
 class MigracaoController extends Controller
 {
@@ -12,5 +14,20 @@ class MigracaoController extends Controller
     {
         Excel::import(new ExcelImport, request()->file('excel'));
 
+        $user = session()->get("utilizador");
+        if($user->tipoUtilizador == 0) {
+            $projetos = Projeto::all();
+            return view('admin/pagInicial', ['projetos' => $projetos]);
+        }
+        else {
+            $projetos = DB::table('projeto')
+                    ->join('projeto_utilizador', 'projeto.id_projeto', '=', 'projeto_utilizador.id_projeto')
+                    ->select('projeto.id_projeto', 'projeto.regulamento', 'projeto.nome', 'projeto.objetivos', 'projeto.publicoAlvo', 'projeto.observacoes')
+                    ->where([
+                        ['projeto_utilizador.id_utilizador', '=', $user->id_utilizador]
+                        ])
+                    ->get();
+            return view('colaborador/pagInicial', ['projetos' => $projetos]);
+        }
     }
 }
